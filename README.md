b9860aa1bfa0218bdd7e682fdc171e80e9a773bb
rozza368 <rorytheb3st@gmail.com>
2022-03-13 17:24:23 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# Clipper
+
+Clipper aims to make it easy to grab a small part from a video into a new file, all from within mpv.
+It supports Linux but may not work with other operating systems yet.
+
+## Features
+### Current
+- Render clip containing current subtitle line to a file
+
+### To Add
+- Manually select times to render between
+- Allow selecting multiple subtitle lines
+
+### In the Future
+- Select file extension for the clip
+- Configurable export quality
+
+
+## Installation
+### Script Directory
+Linux: `~/.config/mpv/scripts`
+
+### Method 1
+Download the raw file `clipper.lua` and place it into the script directory.
+
+### Method 2
+Clone this repository and symlink `clipper.lua` into the script directory.
+
---
+++ clipper.lua
@@ -0,0 +1,55 @@
+local utils = require("mp.utils")
+
+local homedir
+-- might not work on windows?
+for _, item in ipairs(utils.get_env_list()) do
+    local key, val = string.match(item, "(.*)=(.*)")
+    if key == "HOME" then
+        homedir = val
+    end
+end
+
+function get_sub_times()
+    local sub_start = mp.get_property_number("sub-start")
+    local sub_end = mp.get_property_number("sub-end")
+    return {sub_start, sub_end}
+end
+
+function render_clip(start_time, end_time, name)
+    local basedir = homedir
+    local outfile = string.format("%s/Videos/%s.mp4", basedir, name)
+    local infile = mp.get_property_osd("filename")
+    print(infile, "to", outfile)
+    local result = mp.commandv(
+        "run", "ffmpeg",
+        "-n", -- no overwrite
+        "-loglevel", "quiet",
+        "-ss", tostring(start_time),
+        "-to", tostring(end_time),
+        "-i", infile,
+        outfile
+    )
+
+    if result then
+        mp.osd_message(string.format("Rendered %s - %s", start_time, end_time))
+    else
+        mp.osd_message("Failed to render clip")
+    end
+end
+
+function show_sub_time()             
+    local sub_start = get_sub_times()[1]
+    local sub_end = get_sub_times()[2]
+    mp.osd_message(string.format("%s - %s", sub_start, sub_end))
+end
+
+function render_current_sub()
+    local filename = mp.get_property_osd("filename/no-ext")
+    local sub_start = get_sub_times()[1]
+    local sub_end = get_sub_times()[2]
+    render_clip(sub_start, sub_end, string.format("%s %s-%s", filename, sub_start, sub_end))
+end
+
+mp.add_key_binding('g', "show_sub_time", show_sub_time)
+mp.add_key_binding('G', "render_current_sub", render_current_sub)
+
